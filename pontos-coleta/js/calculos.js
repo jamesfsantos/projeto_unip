2892e3eb9539e2192873dd7a6f3e014bc201894f
James Santos <jamesf.santos@hotmail.com>
2026-05-08 16:10:50 -0300

    fix: Adicionando comentarios uteis para o entendimento do codigo.

--- pontos-coleta/js/calculos.js
+++ pontos-coleta/js/calculos.js
@@ -1,4 +1,8 @@
-//Fórmula de Haversine
+
+//Fórmula de Haversine 
+/*
+  Essa função executa toda a formula de Haversine, nela é possivel você descobrir a distância de dois pontos de qualquer lugar do planeta Terra!
+ */
 export function calcularDistancia(lat1, lon1, lat2, lon2) {
   const R = 6371; //Raio da Terra
   const dLat = ((lat2 - lat1) * Math.PI) / 180;
@@ -15,7 +19,8 @@ export function calcularDistancia(lat1, lon1, lat2, lon2) {
   return R * c;
 }
 
-export function encontrarPontosProximos(latUsuario, lngUsuario, listaPontos) {
+// Essa função serve para encontrar
+export function encontrarPontosProximos(latUsuario, lngUsuario, listaPontos) { //ListaPontos armazena aquele JSON...
   const pontosComDistancia = listaPontos.map((ponto) => {
     const dist = calcularDistancia(
       latUsuario,
@@ -26,6 +31,10 @@ export function encontrarPontosProximos(latUsuario, lngUsuario, listaPontos) {
     return { ...ponto, distancia: dist };
   });
 
-  pontosComDistancia.sort((a, b) => a.distancia - b.distancia);
-  return pontosComDistancia.slice(0, 3);
+  pontosComDistancia.sort((a, b) => a.distancia - b.distancia); // .sort() Organiza os elementos de uma lista! 
+  //a.distancia - b.distancia --> é feita a verificação em ordem crescente(Se o resultado der negativo, o ponto a é mais perto que o b e o colca antes na fila)
+  //Se for positivo o resultado --> o ponto b é o mais perto e ele assume a frente
+  // Ao final da lista o ponto do indice[0](primeiro) será o mais proximo da localização
+  return pontosComDistancia.slice(0, 3); //Slice define até onde pode ser exibida a lista, ou seja irá exibir os indices: [0], [1], [2]... a partir do [3] não exibe.
+  // Ou seja o slice é o limita a quantidade de itens!
 }
--- pontos-coleta/js/main.js
+++ pontos-coleta/js/main.js
@@ -1,53 +1,64 @@
+/* Importações para utilizar arquivos externos */
 import { map, usuarioIcon } from "./mapa.js";
-import { encontrarPontosProximos } from "./calculos.js";
+import { encontrarPontosProximos } from "./calculos.js"; 
 
-let dadosDosPontos = [];
+let dadosDosPontos = []; // Variavel global para utilizar em outras funções
+
 async function carregarPontos() {
-  const resposta = await fetch("./dados/coletas.json");
-  dadosDosPontos = await resposta.json();
+  const resposta = await fetch("./dados/coletas.json"); //Pega o JSON dos pontos que criei.
+  dadosDosPontos = await resposta.json(); // converte para JSON para ser possivel pegar suas propiedades
   dadosDosPontos.forEach((p) => {
     L.marker([p.lat, p.lng]).addTo(map).bindPopup(`<b>${p.nome}</b>`);
   });
 }
+/*
+    O JSON segue a ideia de Chave = "valor"
+    Quando você passa p.nome, que nem no exemplo do forEach ele pega o VALOR do p.nome, 
+    que pode ser qualquer coisa que foi definida
+*/
+
+
+// Atualizar interface recebe a nova localização (Ou do GPS ou do Campo de busca) 
+// e ela coordena todas as mudanças visuais que devem acontecer no mapa e na lista lateral...
 
 function atualizarInterface(lat, lng, titulo) {
-  map.flyTo([lat, lng], 15);
-  L.marker([lat, lng], { icon: usuarioIcon })
-    .addTo(map)
-    .bindPopup(titulo)
-    .openPopup();
+  map.flyTo([lat, lng], 15); //Vai pra localização e da um zoom...
+  L.marker([lat, lng], { icon: usuarioIcon }) // Ele cria um novo marcador para na posição exata do usuario (icon: usuarioIcon ==> Icone que vem do ./mapa.js) 
+    .addTo(map)  //Adiciona ao MAPA
+    .bindPopup(titulo) // Cria um popup que exibe o titulo
+    .openPopup(); // Abre apos adicionar.
 
-  const proximos = encontrarPontosProximos(lat, lng, dadosDosPontos);
-  const listaUl = document.getElementById("lista");
-  listaUl.innerHTML = "";
+  const proximos = encontrarPontosProximos(lat, lng, dadosDosPontos); // É passada a localizacao atual e a lista .json que temos. É chamada uma função da ./calculos.js
+  const listaUl = document.getElementById("lista"); //Criamo uma lista sem ordem
+  listaUl.innerHTML = ""; // Inicialmente vazio, mas quando a função é chamada ela lista a lista para vir outra....
 
-  proximos.forEach((p) => {
-    const li = document.createElement("li");
-    li.innerHTML = `<strong>${p.nome}</strong><br><small>${p.distancia.toFixed(2)} km</small>`;
-    li.onclick = () => map.flyTo([p.lat, p.lng], 17);
-    listaUl.appendChild(li);
+  proximos.forEach((p) => { // Loop ele percorre os 3 endereços pré-definidos na função encontrarPontosProximos();
+    const li = document.createElement("li"); // Cria um elemento li
+    li.innerHTML = `<strong>${p.nome}</strong><br><small>${p.distancia.toFixed(2)} km</small>`; // Na lista é exibido o nome da localização e a distância da sua localização até ela!
+    li.onclick = () => map.flyTo([p.lat, p.lng], 17); // Quando você clica no endereço o mapa redireciona até o endereço clicado....
+    listaUl.appendChild(li); // adiciona essas Li dentro da UL
   });
 }
 
 
-document.getElementById("btn-location").addEventListener("click", () => {
+document.getElementById("btn-location").addEventListener("click", () => { // Botão + Ação ao executar botao da Localização Atual... Essa é pega pelo navegador!
   navigator.geolocation.getCurrentPosition((pos) => {
     atualizarInterface(pos.coords.latitude, pos.coords.longitude, "Você");
   });
 });
 
-document.getElementById("btn-buscar").addEventListener("click", async () => {
-  const query = document.getElementById("input-endereco").value;
-  const res = await fetch(
-    `https://nominatim.openstreetmap.org/search?format=json&q=${query}&countrycodes=br&limit=1`,
+document.getElementById("btn-buscar").addEventListener("click", async () => { //  Botão + Ação no botao da busca, 
+  const query = document.getElementById("input-endereco").value; // ao clicar ele pega o texto digitado no input-endereco
+  const res = await fetch( // FETCH ==> Realiza busca na API OpenStreetMap, é como se ele entrasse no site e jogasse o endereço la e retornasse a latitude e longitude... Mas feito via API REST (https)
+    `https://nominatim.openstreetmap.org/search?format=json&q=${query}&countrycodes=br&limit=1`, // ?format=json é o jeito que o dado irá retornar, q=${query} é o texto que o usuario digitou. &countrycodes=br(é pego no BRASIL) limit=1(Apenas um retorno)
   );
-  const dados = await res.json();
-  if (dados.length > 0)
+  const dados = await res.json(); // Converte oque pegou para JSON.
+  if (dados.length > 0) // Verifica se foi pego algum dado.
     atualizarInterface(
-      parseFloat(dados[0].lat),
-      parseFloat(dados[0].lon),
-      "Localização",
+      parseFloat(dados[0].lat), // Caso venha mais de um sempre pega a 1º lat, 
+      parseFloat(dados[0].lon), // Caso venha mais de um sempre pega a 1º lon, 
+      "Localização", // --> Texto exibido no pop-up
     );
 });
 
-carregarPontos();
+carregarPontos(); // Chamada da função por ultimo quando todos os elementos estiverem carregados.
